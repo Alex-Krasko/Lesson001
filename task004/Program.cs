3e077bdad9875efd1c30bf4e2b4ef9699bbfe390
Alex_Krasko <C:\Users\Admin\AppData\Roaming\The Bat!>
2021-11-04 21:01:28 +0300

    Tasks changed

---
+++ task004/Program.cs
@@ -0,0 +1,20 @@
+﻿int a1;
+int b1;
+int c1;
+int r;
+Console.WriteLine("Поиск максимального из трех");
+Console.Write("Введите число 1: ");
+string? a = Console.ReadLine();
+Console.Write("Введите число 2: ");
+string? b = Console.ReadLine();
+Console.Write("Введите число 3: ");
+string? c = Console.ReadLine();
+a1=Convert.ToInt32(a);
+b1=Convert.ToInt32(b);
+c1=Convert.ToInt32(c);
+
+if (a1>b1) {r = a1;}
+else {r = b1;}
+if (r>c1) {}
+else {r = c1;}
+Console.WriteLine("Максимальное число: "+r);
