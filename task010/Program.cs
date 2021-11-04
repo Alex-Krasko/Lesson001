5b34f024a9901ffb396eef3d836014853ea56e48
Alex_Krasko <C:\Users\Admin\AppData\Roaming\The Bat!>
2021-11-04 22:00:35 +0300

    task010 added

---
+++ task010/Program.cs
@@ -0,0 +1,12 @@
+﻿int i = 1;
+int a;
+int b = 0;
+Console.Write("Введите число: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Сумма чисел от 1 до "+a);
+while(i<=a)
+{
+    b += i;
+    i++;
+}
+Console.WriteLine("Сумма чисел равна "+b);
