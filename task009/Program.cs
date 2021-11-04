0955037d563a787b72e53020491e87e925b27f84
Alex_Krasko <C:\Users\Admin\AppData\Roaming\The Bat!>
2021-11-04 21:55:05 +0300

    task009 added

---
+++ task009/Program.cs
@@ -0,0 +1,10 @@
+﻿int i = 1;
+int a;
+Console.Write("Введите число: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Кубы чисел от 1 до "+a);
+while(i<=a)
+{
+    Console.Write(Math.Pow(i,3)+" ");
+    i++;
+}
