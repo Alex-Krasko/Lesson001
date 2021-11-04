852d160482ac7e7070bd803ae048936c2bf60cb7
Alex_Krasko <C:\Users\Admin\AppData\Roaming\The Bat!>
2021-11-04 21:34:53 +0300

    task005 edited

---
+++ task005/Program.cs
@@ -0,0 +1,9 @@
+﻿int a;
+Console.WriteLine("Введите число: ");
+a = Convert.ToInt32(Console.ReadLine());
+if(a%7==0|a%23==0)
+{
+    Console.WriteLine("Число кратно 7 и 23!");
+    Console.WriteLine("Условие выполняется.");
+}
+else {Console.WriteLine("Условие не выполняется.");}
