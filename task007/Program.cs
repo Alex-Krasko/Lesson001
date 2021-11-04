d54edadf3bd3b53d35eea022647b64eb92166ce7
Alex_Krasko <C:\Users\Admin\AppData\Roaming\The Bat!>
2021-11-04 21:45:02 +0300

    task007 added

---
+++ task007/Program.cs
@@ -0,0 +1,15 @@
+﻿int a;
+int b;
+Console.Write("Введите число 1: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+b = Convert.ToInt32(Console.ReadLine());
+if (Math.Pow(b,2)==a)
+{
+    Console.WriteLine("Число 1 является квадратом числа 2");
+}
+if (Math.Pow(a,2)==b)
+{
+    Console.WriteLine("Число 2 является квадратом числа 1");
+}
+else {Console.WriteLine("Ошибка.");}
