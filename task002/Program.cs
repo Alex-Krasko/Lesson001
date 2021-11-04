a42b489d944be83d7aace9ffd0d1640a00d8149d
Alex_Krasko <C:\Users\Admin\AppData\Roaming\The Bat!>
2021-11-04 21:07:11 +0300

    task002 edited

--- task002/Program.cs
+++ task002/Program.cs
@@ -1,2 +1,9 @@
 ﻿int a;
 int b;
+Console.Write("Введите число 1: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+if (a>b) {Console.WriteLine("Число 1 больше числа 2");}
+else {Console.WriteLine("Число 2 больше числа 1");}
