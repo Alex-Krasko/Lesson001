b9bb8a37671d5723673e5690d56ad07acfab0098
Alex_Krasko <C:\Users\Admin\AppData\Roaming\The Bat!>
2021-11-04 21:39:48 +0300

    task006 added

---
+++ task006/Program.cs
@@ -0,0 +1,8 @@
+﻿int a;
+Console.WriteLine("Введите число: ");
+a = Convert.ToInt32(Console.ReadLine());
+if (a==6|a==7)
+{
+    Console.WriteLine("Это выходной!");
+}
+else {Console.WriteLine("Это рабочий день (");}
