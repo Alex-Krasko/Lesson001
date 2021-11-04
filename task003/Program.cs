e200e5c36028482f10ba520f29f930e2d7a3acc0
Alex_Krasko <C:\Users\Admin\AppData\Roaming\The Bat!>
2021-11-04 21:18:00 +0300

    task003 edited

--- task003/Program.cs
+++ task003/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿int a;
+string a1 = "Понедельник!";
+string a2 = "Вторник!";
+string a3 = "Среда!";
+string a4 = "Четверг!";
+string a5 = "Пятница!";
+string a6 = "Суббота!";
+string a7 = "Воскресенье!";
+Console.WriteLine("Введите номер дня недели: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Это же ");
+    if(a==1)
+    {
+        Console.WriteLine(a1);
+    }
+    if(a==2)
+    {
+        Console.WriteLine(a2);
+    }
+    if(a==3)
+    {
+        Console.WriteLine(a3);
+    }
+    if(a==4)
+    {
+        Console.WriteLine(a4);
+    }
+    if(a==5)
+    {
+        Console.WriteLine(a5);
+    }
+    if(a==6)
+    {
+        Console.WriteLine(a6);
+    }
+    if(a==7)
+    {
+        Console.WriteLine(a7);
+    }
+
