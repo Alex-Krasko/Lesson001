043e5e7bdb2465989658661aafba2928197638d5
Alex_Krasko <C:\Users\Admin\AppData\Roaming\The Bat!>
2021-11-04 21:51:16 +0300

    task008 added

---
+++ task008/Program.cs
@@ -0,0 +1,10 @@
+﻿int i = 1;
+int a;
+Console.Write("Введите число: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Квадраты чисел от 1 до "+a);
+while(i<=a)
+{
+    Console.Write(Math.Pow(i,2)+" ");
+    i++;
+}
