6990d464da096f9963d6ee51e50f4c03ae9169c3
Alex_Krasko <C:\Users\Admin\AppData\Roaming\The Bat!>
2021-11-05 15:33:03 +0300

    task16 added

---
+++ task016/Program.cs
@@ -0,0 +1,7 @@
+﻿int a;
+string b = string.Empty;
+
+Console.Write("Введите число: ");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToString(a, 2);
+Console.WriteLine("Число в двоичном виде: "+b);
