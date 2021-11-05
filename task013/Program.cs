6b8ab0507b857da9f547270b0b94171a2b7c424c
Alex_Krasko <C:\Users\Admin\AppData\Roaming\The Bat!>
2021-11-05 15:05:56 +0300

    task013 added

---
+++ task013/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = new int[12];
+int sum = 0;
+for (int i=0;i<array.Length;i++)
+{
+    array[i] = new Random().Next(0,10);
+    Console.Write(array[i]+" ");
+}
+Console.WriteLine();
+for (int j=0;j<array.Length;j++)
+{
+    sum += array[j];
+}
+Console.WriteLine("Сумма чисел массива: "+sum);
