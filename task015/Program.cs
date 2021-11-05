30b0994cc054350b7cebbaf5fc9b29fcee00c261
Alex_Krasko <C:\Users\Admin\AppData\Roaming\The Bat!>
2021-11-05 15:24:42 +0300

    task015 added

---
+++ task015/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] array = new int[6];
+for(int i=0;i<array.Length;i++){
+    Console.Write("Введите число "+(i+1)+": ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+int a = 0;
+for(int i=0;i<array.Length;i++){
+    if(array[i]>0) {a++;}
+}
+Console.WriteLine("Введено "+a+" положительных чисел.");
