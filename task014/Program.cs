b9c0f8e90fd1c0dc27c0a780d002a52ffd5b9f69
Alex_Krasko <C:\Users\Admin\AppData\Roaming\The Bat!>
2021-11-05 15:16:21 +0300

    task014 added

---
+++ task014/Program.cs
@@ -0,0 +1,14 @@
+﻿int a;
+int b;
+int c;
+Console.WriteLine("Введите число 1: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 3: ");
+c = Convert.ToInt32(Console.ReadLine());
+if (a+b>c&a+c>b&b+c>a)
+{
+    Console.WriteLine("Числа являются сторонами треугольника.");
+}
+else Console.WriteLine("Числа НЕ являются сторонами треугольника.");
