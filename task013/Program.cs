3de651f867e1e80818d6e027f5ae383600466ab9
Alex_Krasko <C:\Users\Admin\AppData\Roaming\The Bat!>
2021-11-11 21:38:19 +0300

    task013 edited

--- task013/Program.cs
+++ task013/Program.cs
@@ -1,13 +1,19 @@
 ﻿int[] array = new int[12];
 int sum = 0;
+int sum1 = 0;
 for (int i=0;i<array.Length;i++)
 {
-    array[i] = new Random().Next(0,10);
+    array[i] = new Random().Next(-9,10);
     Console.Write(array[i]+" ");
 }
 Console.WriteLine();
 for (int j=0;j<array.Length;j++)
 {
+    if (array[j] > 0)
+    {
     sum += array[j];
+    }
+    else { sum1+= array[j];}
 }
-Console.WriteLine("Сумма чисел массива: "+sum);
+Console.WriteLine("Сумма положительных чисел массива: "+sum);
+Console.WriteLine("Сумма отрицательных чисел массива: "+sum1);
