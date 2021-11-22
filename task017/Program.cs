1eed11db95960b38ab36a9bc375e016ea9ddd7c0
Alex_Krasko <C:\Users\Admin\AppData\Roaming\The Bat!>
2021-11-22 22:22:00 +0300

    task018 added

---
+++ task017/Program.cs
@@ -0,0 +1,26 @@
+﻿void SetArray(int[,] a)
+{
+for(int i=0;i<a.GetLength(0);i++)
+    for(int j=0;j<a.GetLength(1);j++)
+    {
+        a[i,j]=i*10+j;
+    }
+}
+
+void Print(int[,] a)
+{
+for(int i=0;i<a.GetLength(0);i++) {
+
+    for(int j=0;j<a.GetLength(1);j++)
+    {
+        Console.Write($"{a[i,j],4}");   
+    }
+    Console.WriteLine();
+}
+}
+
+int[,] a;
+a=new int[10,10];
+
+SetArray(a);
+Print(a);
---
+++ task018/Program.cs
@@ -0,0 +1,54 @@
+﻿int n = 10;
+int m = 10;
+int [,] array = new int [n,m];
+int sum = 0;
+double multi = 1;
+
+void Print(int[,] a)
+{
+for(int i=0;i<a.GetLength(0);i++) {
+
+    for(int j=0;j<a.GetLength(1);j++)
+    {
+        Console.Write($"{a[i,j],4}");   
+    }
+    Console.WriteLine();
+}
+}
+
+int[,] Fill(){
+for(int i =0;i<array.GetLength(0);i++)
+    for(int j=0;j<array.GetLength(1);j++)
+    {
+        array[i,j] = new Random().Next(1,100);;
+    }
+return array;
+}
+
+int Sum()
+{
+for(int i =0;i<array.GetLength(0);i++){
+    for(int j=0;j<array.GetLength(1);j++)
+    {
+        sum +=array[i,j];
+    }
+}
+return sum;
+}
+
+double Proizv()
+{
+for(int i =0;i<array.GetLength(0);i++)
+    {
+        for(int j=0;j<array.GetLength(1);j++)
+            {
+                multi *= array[i,j];
+            }
+    }
+return multi;
+}
+
+Fill();
+Print(array);
+Console.WriteLine("Сумма: "+Sum());
+Console.WriteLine("Произведение: "+Proizv());
